c315e79a402453e3c31f8fdeeb25815e2491d4ad
Atakan Arslan <atak.arslan@hotmail.com>
2024-01-03 19:48:49 +0300

    agm

--- AnonimGrupMesaj/Hub/ChatHub.cs
+++ AnonimGrupMesaj/Hub/ChatHub.cs
@@ -4,9 +4,17 @@ namespace AnonimGrupMesaj.Hub
 {
     public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
     {
+        private readonly IDictionary<string, UserRoomConnection> _connections;
+
+        public ChatHub(IDictionary<string, UserRoomConnection> connections)
+        {
+            _connections = connections;
+        }
+
         public async Task JoinRoom(UserRoomConnection userRoomConnection)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName: userRoomConnection.Room!);
+            _connections[Context.ConnectionId] = userRoomConnection;
             await Clients.Group(userRoomConnection.Room!).SendAsync(method: "ReceiveMessage", arg1: "Lets Program Bot", arg2: $"{userRoomConnection.User} has Joined the Group");
         }
     }

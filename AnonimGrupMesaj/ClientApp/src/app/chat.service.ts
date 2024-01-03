c1ac4e0265aa6a48e0401a5e37ec4ac3fa34cb65
Atakan Arslan <atak.arslan@hotmail.com>
2024-01-03 21:51:21 +0300

    agm

---
+++ AnonimGrupMesaj/ClientApp/src/app/chat.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  public connection : any = new signalR.HubConnectionBuilder().withUrl("http://localhost:4200/chat").configureLogging(signalR.LogLevel.Information).build();
+  constructor() { }
+
+  //start connection
+  public async start(){
+    try{
+      await this.connection.start();
+    }catch (error){
+      console.log(error);  
+    }
+  }
+
+  //Join Room
+  public async JoinRoom(user: string, room: string){
+    return this.connection.invoke("JoinRoom", {user, room})
+  }
+
+
+  //Send Messages
+  public async sendMessage(message: string){
+    return this.connection.invoke("SendMessage", message)
+  }
+
+
+  //Leave
+  public async leaveChat()
+  {
+    return this.connection.stop();
+  }
+}

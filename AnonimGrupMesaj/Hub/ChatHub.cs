17dadb502ce8bdbede6933398123a2a618a7b4c7
Atakan Arslan <atak.arslan@hotmail.com>
2024-01-03 20:03:57 +0300

    agm

--- AnonimGrupMesaj/Hub/ChatHub.cs
+++ AnonimGrupMesaj/Hub/ChatHub.cs
@@ -16,6 +16,7 @@ namespace AnonimGrupMesaj.Hub
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName: userRoomConnection.Room!);
             _connections[Context.ConnectionId] = userRoomConnection;
             await Clients.Group(userRoomConnection.Room!).SendAsync(method: "ReceiveMessage", arg1: "Lets Program Bot", arg2: $"{userRoomConnection.User} has Joined the Group");
+
         }
 
         public async Task SendMessage(string message)
@@ -24,6 +25,26 @@ namespace AnonimGrupMesaj.Hub
             {
                 await Clients.Groups(userRoomConnection.Room!).SendAsync(method: "ReceiveMessage", arg1: userRoomConnection.User, arg2: message, arg3: DateTime.Now);
             }
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exp)
+        {
+            if (!_connections.TryGetValue(Context.ConnectionId,
+                out UserRoomConnection roomConnection))
+            {
+                return base.OnDisconnectedAsync(exp);
+            }
+            Clients.Group(roomConnection.Room!).SendAsync(method:
+                "ReceiveMessage", arg1: "Lets Program bot", arg2: $"{roomConnection.User} has left the Group");
+            SendConnectedUser(roomConnection.Room!);
+            return base.OnDisconnectedAsync(exp);
+        }
+
+        public Task SendConnectedUser(string room)
+        {
+            var users = _connections.Values
+                .Where(u => u.Room == room).Select(s => s.User);
+            return Clients.Group(room).SendAsync(method: "ConnectedUser", users);
         }
     }
 }

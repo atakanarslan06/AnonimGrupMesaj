8cc8cfdbaca80cd43d5798520f1c9e6a04e91f92
Atakan Arslan <atak.arslan@hotmail.com>
2024-01-03 21:04:01 +0300

    agm

--- AnonimGrupMesaj/ClientApp/src/app/app.module.ts
+++ AnonimGrupMesaj/ClientApp/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
+import { JoinRoomComponent } from './join-room/join-room.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    JoinRoomComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
